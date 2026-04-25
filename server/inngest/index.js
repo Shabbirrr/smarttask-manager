052f75d7742976177d66520b2127ef7a5de13df8
Shabbirrr <shabbirshah722@gmail.com>
2026-04-26 03:18:23 +0530

    initial commit

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -5,8 +5,8 @@ export const inngest = new Inngest({ id: "Project Management" });
 
 //Inngest function to save user data
 const syncUserCreation= inngest.createFunction(
-    { id: "Sync User Creation" },
-    { event: "clerk/user.created" },
+    { id: "Sync User Creation",
+      event: "clerk/user.created" },
     async ({ event}) => {
         const { data } = event;
         await prisma.user.create({
@@ -23,8 +23,8 @@ const syncUserCreation= inngest.createFunction(
 
 //Inngest function to delete user data
 const syncUserDeletion = inngest.createFunction(
-    { id: "Sync User Deletion" },
-    { event: "clerk/user.deleted" },
+    { id: "Sync User Deletion" ,
+      event: "clerk/user.deleted" },
     async ({ event}) => {
         const { data } = event;
         await prisma.user.delete({
@@ -37,8 +37,8 @@ const syncUserDeletion = inngest.createFunction(
 
 //Inngest function to update user data
 const syncUserUpdate = inngest.createFunction(
-    { id: "Sync User Update" },
-    { event: "clerk/user.updated" },
+    { id: "Sync User Update" ,
+      event: "clerk/user.updated" },
     async ({ event}) => {
         const { data } = event;
         await prisma.user.update({
